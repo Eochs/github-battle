c63ee050e3522decac20bb099c03905334efa755
Eochs <slochastic@gmail.com>
2016-03-23 22:05:38 -0700

    still messing with routes

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -18,6 +18,18 @@ var PromptContainer = React.createClass({
     this.setState({
       username: ''
     });
+
+    if (this.props.routeParams.playerOne) {
+      this.context.router.push({
+        pathname: '/battle',
+        query: {
+          playerOne: this.props.routeParams.playerOne,
+          playerTwo: this.state.username
+        }
+      })
+    } else {
+      this.context.router.push('/playerTwo' + this.state.username);
+    }
   },
 
   render: function(){
